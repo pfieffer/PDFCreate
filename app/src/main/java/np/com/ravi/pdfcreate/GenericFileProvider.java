59c07beb02c1fe4a01d80954074e1db4be5cbe24
RaviGarbujaPun <cbpmgr@gmail.com>
2018-01-04 12:48:59 +0545

    Implement PDF creation using iTextg Library

---
+++ app/src/main/java/np/com/ravi/pdfcreate/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package np.com.ravi.pdfcreate;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by ravi on 1/4/18.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
